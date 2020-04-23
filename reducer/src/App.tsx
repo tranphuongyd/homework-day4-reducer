85431eea0444c4b77d3308552b1f912460427837
tranphuongyd <tranphuongyd@gmail.com>
2020-04-23 22:49:08 +0700

    remove notification

--- reducer/src/App.tsx
+++ reducer/src/App.tsx
@@ -2,16 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'antd/dist/antd.css';
-import {NotificationSetting} from './components/notification-setting/notification-setting'
-import {OnlineOffline} from './components/online-offline/online-offline'
 import {Post} from './components/post/post-list'
 import { PostListReducer } from './components/post/post-list-reducer';
 
 function App() {
   return (
     <div className="App">
-      <OnlineOffline/>
-      <NotificationSetting />
       <Post/>
       <PostListReducer />
     </div>
